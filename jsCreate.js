8f2ec6c54b8e36d115d4f7832943104afea15f1b
SamuelMazuim <samuelmazuim@hotmail.com>
2022-03-30 21:51:16 -0300

    fix old feature

--- jsCreate.js
+++ jsCreate.js
@@ -1,60 +1,59 @@
-let createUser = document.getElementById("createUser");
-let createPassword = document.getElementById("exampleInputPassword1");
-let btCreate = document.getElementById("createAccount");
+let createUser = document.getElementById("createUser")
+let createPassword = document.getElementById("exampleInputPassword1")
+let btCreate = document.getElementById("createAccount")
 
-btCreate.onclick = () => create();
+btCreate.onclick = () => create()
 
 let userId = () => {
-    const users = allAccountsLocalStorage();
+    const users = allAccountsLocalStorage()
     if (users.length == 0) {
-        return 1;
+        return 1
     }
-    const userSlice = users.slice(-1);
-    let user = userSlice[0];
-    return user.id + 1;
-};
+    const userSlice = users.slice(-1)
+    let user = userSlice[0]
+    return user.id + 1
+}
 
 function create() {
-    const users = allAccountsLocalStorage();
+    const users = allAccountsLocalStorage()
     let newUser = {
         id: userId(),
         name: createUser.value,
         password: createPassword.value,
-    };
+    }
     if (!userNameCheck(newUser.name)) {
-        return alert("This username is already in use.");
+        return alert("This username is already in use.")
     }
     if ((createUser.value == !!"") | (createPassword.value == !!"")) {
-        return alert("Please don't let any empty fields.");
+        return alert("Please don't let any empty fields.")
     }
-    localStorage.setItem("accounts", JSON.stringify([...users, newUser]));
+    localStorage.setItem("accounts", JSON.stringify([...users, newUser]))
 
-    userId++;
-    const setInLocalStorage = (taskArray) => localStorage.setItem("userTasks" + newUser.id, "[]");
+    userId++
 
     const getFromLocalStorage = () =>
-        JSON.parse(localStorage.getItem("userTasks" + newUser.id)) ?? [];
+        JSON.parse(localStorage.getItem("userTasks" + newUser.id)) ?? []
 
-    setInLocalStorage();
+    setInLocalStorage()
 
-    newUser.tasks.push(getFromLocalStorage());
+    newUser.tasks.push(getFromLocalStorage())
 }
 
 function userNameCheck(userName) {
-    const users = allAccountsLocalStorage();
+    const users = allAccountsLocalStorage()
     if (
         users.findIndex(function (user) {
-            return user.name === userName;
+            return user.name === userName
         }) != "-1"
     ) {
-        return false;
+        return false
     }
-    return true;
+    return true
 }
 
 function allAccountsLocalStorage() {
-    const users = localStorage.getItem("accounts");
-    return JSON.parse(users) ?? [];
+    const users = localStorage.getItem("accounts")
+    return JSON.parse(users) ?? []
 }
 
-btCreate.onclick = () => create();
+btCreate.onclick = () => create()
